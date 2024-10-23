a94b6f6f77e321778e0b9e18a02c1eedd056b807
Joel Andrade <joelcesar09@gmail.com>
2024-10-23 20:58:18 -0300

    Inclusão do Repository da Entidade "RegistroPonto"

---
+++ ControlePontoAPI/Repositories/Interfaces/IRegistroPontoRepository.cs
@@ -0,0 +1,12 @@
+﻿using ControlePontoAPI.Models;
+
+namespace ControlePontoAPI.Repositories.Interfaces;
+
+public interface IRegistroPontoRepository
+{
+    Task<IEnumerable<RegistroPonto>> GetAllAsync();
+    Task<RegistroPonto?> GetByIdAsync(int id);
+    Task AddAsync(RegistroPonto registroPonto);
+    Task<RegistroPonto> UpdateAsync(RegistroPonto registroPonto);
+    Task DeleteAsync(RegistroPonto registroPonto);
+}
---
+++ ControlePontoAPI/Repositories/RegistroPontoRepository.cs
@@ -0,0 +1,46 @@
+﻿using ControlePontoAPI.Data;
+using ControlePontoAPI.Models;
+using ControlePontoAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlePontoAPI.Repositories;
+
+public class RegistroPontoRepository : IRegistroPontoRepository
+{
+    private readonly AppDbContext _context;
+
+    public RegistroPontoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<RegistroPonto>> GetAllAsync()
+    {
+        return await _context.RegistrosPonto.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<RegistroPonto?> GetByIdAsync(int id)
+    {
+        return await _context.RegistrosPonto.FirstOrDefaultAsync(r => r.Id == id);
+    }
+
+    public async Task AddAsync(RegistroPonto registroPonto)
+    {
+        await _context.RegistrosPonto.AddAsync(registroPonto);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<RegistroPonto> UpdateAsync(RegistroPonto registroPonto)
+    {
+        _context.Entry(registroPonto).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return registroPonto;
+    }
+
+    public async Task DeleteAsync(RegistroPonto registroPonto)
+    {
+        _context.RegistrosPonto.Remove(registroPonto);
+        await _context.SaveChangesAsync();
+    }
+}
